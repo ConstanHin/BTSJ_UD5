2ca083ee3a234245084d99c511aa725f697b3219
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-07 11:04:02 +0200

    añadido punto siete

---
+++ src/pk_TA5_7/Siete.java
@@ -0,0 +1,16 @@
+package pk_TA5_7;
+
+public class Siete {
+
+	public static void main(String[] args) {
+		// Mostrar los numeros del 1 al 100 por consola
+		
+		int i = 1;
+		while(i <= 100) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
