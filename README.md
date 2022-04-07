0fa8e1b905f39ef4c15bf68840760393754f2aed
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-07 09:01:49 +0200

    Añadido punto uno

---
+++ README.md
@@ -0,0 +1,3 @@
+# BTSJ_UD5
+
+## C2 - Desenvolupament orientat a objectes amb Java
---
+++ src/pk_TA5_1/Uno.java
@@ -0,0 +1,18 @@
+package pk_TA5_1;
+
+public class Uno {
+
+	public static void main(String[] args) {
+		
+		int first = 2;
+		int second = 3;
+		
+		System.out.println("First value:" +(first));
+		System.out.println("Second value:" +(second));
+		System.out.println("First grater than second:" +(first > second));
+		System.out.println("First lower than second:" +(first < second));
+		System.out.println("First equal to second:" +(first == second));
+
+	}
+
+}
