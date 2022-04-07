daa70977fd57a80f1ecb9db29030b9afe142ccf6
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-07 11:30:58 +0200

    añadido punto 8

---
+++ src/pk_TA5_8/Ocho.java
@@ -0,0 +1,17 @@
+package pk_TA5_8;
+
+import java.util.Iterator;
+
+public class Ocho {
+
+	public static void main(String[] args) {
+		
+		// Mostrar los numeros del 1 al 100 por consola (bucle for)
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
